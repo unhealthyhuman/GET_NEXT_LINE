1c2807a25fbe126f5b37a4ffe3ba1c21c2851ace
Ildiko Jasmin Schmutzer <ischmutz@c2r5p5.42vienna.com>
2023-10-20 17:14:50 +0200

    1

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,28 +6,69 @@
 /*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:15:54 by ischmutz          #+#    #+#             */
-/*   Updated: 2023/10/19 15:40:51 by ischmutz         ###   ########.fr       */
+/*   Updated: 2023/10/20 17:13:48 by ischmutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	*ft_calloc(size_t num, size_t size)
+char	*ft_strchr(const char *s, int c)
 {
-	void	*buffer;
-	size_t	bytesize;
+	int		i;
+	char	*result;
+	char	*s_;
 
-	if (size == 0 || num == 0)
-		return (malloc(0));
-	bytesize = num * size;
-	if (bytesize / num != size || bytesize > 2147483647)
-		return (NULL);
-	buffer = malloc(bytesize);
-	if (buffer != NULL)
+	i = 0;
+	result = NULL;
+	s_ = (char *) s;
+	while (s_[i] != '\0')
 	{
-		ft_memset(buffer, 0, bytesize);
+		if (s_[i] != '\n')
+			i++;
+		else
+		{
+			result = s_ + i;
+			break ;
+		}
 	}
-	return (buffer);
+	return (result);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	i;
+
+	i = 0;
+	if (start >= ft_strlen(s) || len == 0)
+	{
+		subs = ft_calloc(1, 1);
+		return (subs);
+	}
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	subs = ft_calloc(len + 1, sizeof(char));
+	if (subs == NULL)
+		return (NULL);
+	while (i < len && s[start] != '\0')
+	{
+		subs[i] = s[i + start];
+		i++;
+	}
+	subs[i] = '\0';
+	return (subs);
 }
 
 char	*read(fd)
@@ -35,10 +76,10 @@ char	*read(fd)
 	int		bytes_read;
 	char	*tinybuffer;
 
-	tinybuffer = ft_calloc(3 + 1 * sizeof(char)); //*********
+	tinybuffer = malloc(BUFFER_SIZE + 1 * sizeof(char)); //*********
 	if (tinybuffer == NULL)
 		return (NULL);
-	bytes_read = read(fd, tinybuffer, 3);
+	bytes_read = read(fd, tinybuffer, BUFFER_SIZE);
 	if (bytes_read <= 0)
 		return (NULL);
 	return (tinybuffer);
